3deb45194f162a7b0019feeb83a2a5970949ae13
umitcanbal <umitcanbal@gmail.com>
2020-02-27 13:05:22 +0100

    çok bi şey değişmedi, öğleden sonraya geçtik

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    //displays the form to create a new author
+    public function create() {
+        return "hey";
+    }
+}
