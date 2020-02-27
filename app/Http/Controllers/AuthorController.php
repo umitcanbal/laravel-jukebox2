d6592c94d170a48a0a3bc96f0eb51c50f21ae248
umitcanbal <umitcanbal@gmail.com>
2020-02-27 13:24:19 +0100

    layout created, edit file created, they both are connected and rendered by a function defined in Autocontroller and function is called by route (web.php)

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -8,6 +8,14 @@ class AuthorController extends Controller
 {
     //displays the form to create a new author
     public function create() {
-        return "hey";
+
+
+        //return view("admin/author/edit");
+        //view'ın içindeki edit dosyasını renderlemek istiyorum burada
+
+        $view = view("admin/author/edit");
+        return $view;
+        //$view bi object!!
+        //renderladığım şey aslında layout.blade.php ama o her şeyi saran bi yapı, direkt edit dosyasını koyabilirim burada view'e, o extendlendiği için kendi yolunu bulup, renderlanır.
     }
 }
---
+++ resources/views/admin/author/edit.blade.php
@@ -0,0 +1,12 @@
+
+@extends("admin/layout", [
+  "title" => "create a new author"
+])
+<!-- Burada variable yolladım layout.blade.php'e -->
+
+@section("content")
+<!-- content yerine ne yazarsan yaz. -->
+
+  çalışıyo!!
+
+@endsection 
